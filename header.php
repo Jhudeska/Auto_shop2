606d36868895beed84217737895418e9657b46f8
Jhudeska <jvsignacia@live.nl>
2019-05-10 14:57:34 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Jhudeska | dev.Jeroen_Opracht</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="shortcut icon" type="image/png" href="img/kiss-dashboard.png">
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-light sticky-top navbar-autoshop">
+    <div class="container-fluid">
+        <a href="index.php" class="navbar-brand"><img class="logo" src="img/logo@2x.png" alt=""></a>
+    </div>
+</nav>
+<div class="wrapper">
+   <div class=" col-xs-3 col-sm-6 col-md-12 col-lg-12 col-xl-12 text-center">
+       <header class="header">
+           <div class="heading-primary">
+               <span class="heading-primary-main">Opdracht voor Jhudeska</span>
+               <p class="heading-primary-sub">Het is de bedoeling dat dit volledig weet te realiseren  met HTML 5 /CSS 3 als je eerste opdracht</p>
+           </div>
+       </header>
+   </div>
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php require_once 'header.php';?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 p-0">
+            <div class="model-preview-block">
+                <div class="model-box">
+                    <div class="model-box-index-large model-box-index-large-first">
+                        <div class="modal-title"><h2>FABIA</h2></div>
+                        <div class="-modal-type">Active</div>
+                        <div class="modal-description"> Stoere basis</div>
+                        <div class="modal-price">Vanaf €15.830</div>
+                        <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Fabia's</button>
+                    </div>
+                    <div class="modal-image-large"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 p-0">
+            <div class="model-preview-block">
+                <div class="model-box">
+                    <div class="model-box-index-large model-box-index-large-first">
+                        <div class="modal-title"><h2>FABIA</h2></div>
+                        <div class="-modal-type">Active</div>
+                        <div class="modal-description"> Stoere basis</div>
+                        <div class="modal-price">Vanaf €15.830</div>
+                        <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Fabia's</button>
+                    </div>
+                    <div class="modal-image-large"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 np-0">
+            <div class="model-preview-block">
+                <div class="model-box">
+                    <div class="model-box-index-large model-box-index-large-first">
+                        <div class="modal-title"><h2>FABIA</h2></div>
+                        <div class="-modal-type">Active</div>
+                        <div class="modal-description"> Stoere basis</div>
+                        <div class="modal-price">Vanaf €15.830</div>
+                        <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Fabia's</button>
+                    </div>
+                    <div class="modal-image-large"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid container-settings">
+    <div class="row">
+
+        <div class="model-preview-block">
+            <div class="model-box">
+                <div class="model-box-index-large model-box-index-large-first">
+                    <div class="modal-title"><h2>FABIA</h2></div>
+                    <div class="-modal-type">Active</div>
+                    <div class="modal-description"> Stoere basis</div>
+                    <div class="modal-price">Vanaf €15.830</div>
+                    <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Fabia's</button>
+                </div>
+                <div class="modal-image-large"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+            </div>
+        </div>
+
+        <div class="model-preview-block-middle">
+            <div class="model-box">
+                <div class="model-box-index-small model-box-index-small-middle">
+                    <div class="modal-title"><h2>KODIAQ</h2></div>
+                    <div class="modal-type">Comfort</div>
+                    <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Kodiaq's</button>
+                </div>
+                <div class="modal-image-small"><img src="img/Skoda_Kodiaq.jpg" alt="Kodiaq foto"></div>
+            </div>
+        </div>
+
+
+        <div class="model-preview-block">
+            <div class="model-box">
+                <div class="model-box-index-large model-box-index-large-last">
+                    <div class="modal-title"><h2>OCTAVIA</h2></div>
+                    <div class="-modal-type">Comfort</div>
+                    <div class="modal-description"> Stoere basis</div>
+                    <div class="modal-price">Vanaf €24.890</div>
+                    <button onclick="location.href='car-details-fabia.php'" class="btn btn-white">Bekijk onze Octavia's</button>
+                </div>
+                <div class="modal-image-large"><img src="img/Skodia_Octavia.jpg" alt="Octavia foto"></div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<?php include_once 'footer.php';?>
