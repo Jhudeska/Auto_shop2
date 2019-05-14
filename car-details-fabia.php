7e503cd197bdd76e78c2fee0275465c0e145175d
Jhudeska <jvsignacia@live.nl>
2019-05-14 18:06:09 +0200

    setup grid calculator form

--- car-details-fabia.php
+++ car-details-fabia.php
@@ -1,183 +1,170 @@
 <?php require_once 'header.php';?>
 
-<div class="container px-0 mx-0">
-    <div class="row py-4 my-4">
-        <div class="col-12 col-sm-6 col-md-4 mb-5 py-0 my-0">
-            <div class="model-preview-block model-preview-block-details">
-                <div class="model-preview-car-details-page">
-                    <div class="car-details-title">FABIA HATCHBACK</div>
-                    <div class="car-details-type">Active</div>
-                </div>
-                <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
-                <div class="modal-text-box">
-                    <div class="description-bold-special">Standaard op de Active:</div>
-                    <div class="description-special"> Laadruimte tot 1.150 liter</div>
-                    <div class="description-special"> LED-daverlichting</div>
-                    <div class="description-special"> 6 Airbags</div>
-                    <div class="description-bold"> Stoere basis</div>
-                    <div class="description-margin">Vanaf €15.830</div>
-                    <button class="btn btn-green">Bestel deze variant</button>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="model-preview-block model-preview-block-details">
-                <div class="model-preview-car-details-page">
-                    <div class="car-details-title">FABIA HATCHBACK</div>
-                    <div class="car-details-type">Active</div>
-                </div>
-                <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
-                <div class="modal-text-box">
-                    <div class="description-bold-special">Standaard op de Active:</div>
-                    <div class="description-special"> Laadruimte tot 1.150 liter</div>
-                    <div class="description-special"> LED-daverlichting</div>
-                    <div class="description-special"> 6 Airbags</div>
-                    <div class="description-bold"> Stoere basis</div>
-                    <div class="description-margin">Vanaf €15.830</div>
-                    <button class="btn btn-green">Bestel deze variant</button>
+    <div class="container px-0 mx-0">
+        <div class="row py-4 my-4">
+            <div class="col-12 col-sm-6 col-md-4 mb-5 py-0 my-0">
+                <div class="model-preview-block model-preview-block-details">
+                    <div class="model-preview-car-details-page">
+                        <div class="car-details-title">FABIA HATCHBACK</div>
+                        <div class="car-details-type">Active</div>
+                    </div>
+                    <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                    <div class="modal-text-box">
+                        <div class="description-bold-special">Standaard op de Active:</div>
+                        <div class="description-special"> Laadruimte tot 1.150 liter</div>
+                        <div class="description-special"> LED-daverlichting</div>
+                        <div class="description-special"> 6 Airbags</div>
+                        <div class="description-bold"> Stoere basis</div>
+                        <div class="description-margin">Vanaf €15.830</div>
+                        <button class="btn btn-green">Bestel deze variant</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="model-preview-block model-preview-block-details">
-                <div class="model-preview-car-details-page">
-                    <div class="car-details-title">FABIA HATCHBACK</div>
-                    <div class="car-details-type">Active</div>
-                </div>
-                <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
-                <div class="modal-text-box">
-                    <div class="description-bold-special">Standaard op de Active:</div>
-                    <div class="description-special"> Laadruimte tot 1.150 liter</div>
-                    <div class="description-special"> LED-daverlichting</div>
-                    <div class="description-special"> 6 Airbags</div>
-                    <div class="description-bold"> Stoere basis</div>
-                    <div class="description-margin">Vanaf €15.830</div>
-                    <button class="btn btn-green">Bestel deze variant</button>
+
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="model-preview-block model-preview-block-details">
+                    <div class="model-preview-car-details-page">
+                        <div class="car-details-title">FABIA HATCHBACK</div>
+                        <div class="car-details-type">Active</div>
+                    </div>
+                    <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                    <div class="modal-text-box">
+                        <div class="description-bold-special">Standaard op de Active:</div>
+                        <div class="description-special"> Laadruimte tot 1.150 liter</div>
+                        <div class="description-special"> LED-daverlichting</div>
+                        <div class="description-special"> 6 Airbags</div>
+                        <div class="description-bold"> Stoere basis</div>
+                        <div class="description-margin">Vanaf €15.830</div>
+                        <button class="btn btn-green">Bestel deze variant</button>
+                    </div>
                 </div>
             </div>
+
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="model-preview-block model-preview-block-details">
+                    <div class="model-preview-car-details-page">
+                        <div class="car-details-title">FABIA HATCHBACK</div>
+                        <div class="car-details-type">Active</div>
+                    </div>
+                    <div class="modal-car-details-image"><img src="img/Skoda_Fabia.jpg" alt="Fabia foto"></div>
+                    <div class="modal-text-box">
+                        <div class="description-bold-special">Standaard op de Active:</div>
+                        <div class="description-special"> Laadruimte tot 1.150 liter</div>
+                        <div class="description-special"> LED-daverlichting</div>
+                        <div class="description-special"> 6 Airbags</div>
+                        <div class="description-bold"> Stoere basis</div>
+                        <div class="description-margin">Vanaf €15.830</div>
+                        <button class="btn btn-green">Bestel deze variant</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
-    </div>
+        <section class="calculator-section">
+            <div class="container">
+                <div class="row">
 
- <section class="calculator-section">
-     <div class="container">
-         <div class="row">
-             <div class="col">
+                    <div class="calculator-block">
+                        <form action="#" method="get" class="calculator-form col-sm-6">
 
-                 <div class="calculator-block">
-                     <div class="heading-primary">
-                         <h1 class="heading-calc">Ik ontvang graag een offerte Skoda Fabia Hatchback </h1>
-                         <h1 class="heading-calc">Business Edition </h1>
-                     </div>
+                            <div class=" form-row col-12">
+                                <div class="heading-primary">
+                                    <h1 class="heading-calc">Ik ontvang graag een offerte Skoda Fabia Hatchback </h1>
+                                    <h1 class="heading-calc">Business Edition </h1>
+                                </div>
+                            </div>
 
 
-                     <section>
-                         <div class="container">
-                             <div class="row">
-                                 <div class="col-12">
-                                     <div class="container-leaseamount ">
-                                         <p class="font-weight-bold display-4 txt-leaseamount"> €1445 </p>
-                                         <p class="txt-leaseamount"> Is dit uw lease bedrag per maand? </p>
-                                         <p class="txt-leaseamount">Wij willen u graag een verblijvende </p>
-                                         <p class="txt-leaseamount">offerte aanbieden. </p>
-                                     </div>
 
+
+                            <div class="form-row">
+                                <label for="credit-amount" class=" col-4 col-sm-4 col-form-label" disabled>Aanschaf (ex. BTW):</label><br>
+                                <input type="text" class="col-3 col-sm-4 bg-light-gray" name="credit_amount" id="credit-amount">
+                            </div>
+
+                            <div class="form-row">
+                                <label for="amount" class=" col-4 col-sm-4 col-form-label">Aanbetaling:</label><br>
+                                <input type="text" class="col-3 col-sm-4" name="val3" id="amount">
+                            </div>
+
+                            <div class="form-row">
+                                <label for="amount" class=" col-4 col-sm-4 col-form-label">Slottermijn:</label><br>
+                                <input type="text" class="col-3 col-sm-4" name="val3" id="amount">
+                            </div>
+
+                            <div class="form-row">
+                                <label for="months" class="col-4 col-sm-4 col-form-label">Looptijd (mnd):</label><br>
+                                <select  class="col-3 col-sm-4 bg-light-gray" name="val3" id="months">
+                                    <option value="jan">1</option>
+                                    <option value="feb">2</option>
+                                    <option value="mrt">3</option>
+                                    <option value="apr">4</option>
+                                    <option value="mei">5</option>
+                                    <option value="jun">6</option>
+                                    <option value="jul">7</option>
+                                    <option value="aug">8</option>
+                                    <option value="sept">9</option>
+                                    <option value="okt">10</option>
+                                    <option value="nov">11</option>
+                                    <option value="dec">12</option>
+                                </select>
+                            </div>
+
+
+                            <div class=" form-row col-12 ">
+                                    <div class="container-leaseamount ">
+                                        <p class="font-weight-bold display-4 txt-leaseamount"> €1445 </p>
+                                        <p class="txt-leaseamount"> Is dit uw lease bedrag per maand? </p>
+                                        <p class="txt-leaseamount">Wij willen u graag een verblijvende </p>
+                                        <p class="txt-leaseamount">offerte aanbieden. </p>
                                     </div>
-                             </div>
-
-                             <div class="row ">
-                                 <div class="col-12 ">
-                                     <form action="#" method="get" class="calculator-form">
-
-                                         <label>Aanschaf (ex. BTW):</label><br>
-                                         <input type="text" name="val3" id="val3">
-                                         <br>
-
-                                         <label>Aanbetaling:</label><br>
-                                         <input type="text" name="val1" id="val1">
-                                         <br>
-
-                                         <label>Slottermijn:<br></label>
-                                         <input type="text" name="val4" id="val4">
-                                         <br>
-
-                                         <label>Looptijd (mnd):</label><br>
-                                         <input type="text" name="val2" id="val2">
-                                         <br><br>
-                                     </form>
-
-                                 </div>
-                             </div>
+                            </div>
 
 
-                         </div>
+                            <h1 class="txt-form-personal">  VUL JE GEGEVENS IN </h1>
 
+                                <div class="form-group">
+                                    <label  for="aanhef" class="radio form-label">Aanhef<sup>*</sup></label>
+                                    <input type="radio" checked="checked" name="radio" id="aanhef"> Dhr.
+                                    <input type="radio" name="radio" id="aanhef"> Mevr.
+                                    <span class="checkmark"></span>
 
+                                </div>
+                                <div class="formgroup form-group-padding">
+                                    <label for="fname" class="form-label col-4 "">Voorletters</label><sup>*</sup>
+                                    <input class="form-control form-control-sm col-4 " type="text" id="fname" name="firstname" placeholder="Voorletters">
+                                </div>
+                                <div class="formgroup">
+                                    <label for="fname">Achternaam</label><sup>*</sup>
+                                    <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="Achternaam">
+                                </div>
+                                <div class="formgroup">
+                                    <label for="fname">Telefoonnummer</label><sup>*</sup>
+                                    <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="0612345678">
+                                </div>
+                                <div class="formgroup">
+                                    <label for="fname">Emailadres</label><sup>*</sup>
+                                    <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="Emailadres">
+                                </div>
+                                <div class="formgroup">
+                                    <label class="radio">Auto inruilen </label>
+                                    <input type="radio" checked="checked" name="radio"> Ja
+                                    <input type="radio" name="radio"> Nee
+                                    <span class="checkmark"></span>
+                                </div>
 
+                                <button class="btn btn-green">Versturen</button>
+                            </form>
 
-
-
-
-
-
-
-</section>
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="txt-form-personal">  VUL JE GEGEVENS IN </h1>
-                <form action="" method="get" class="calculator-form">
-
-                    <div class="form-group">
-                        <label class="radio">Aanhef<sup>*</sup></label>
-                        <input type="radio" checked="checked" name="radio"> Dhr.
-                        <input type="radio" name="radio"> Mevr.
-                        <span class="checkmark"></span>
-
-                    </div>
-                    <div class="formgroup form-group-padding">
-                        <label for="fname">Voorletters</label><sup>*</sup>
-                        <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="Voorletters">
-                    </div>
-                    <div class="formgroup">
-                        <label for="fname">Achternaam</label><sup>*</sup>
-                        <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="Achternaam">
-                    </div>
-                    <div class="formgroup">
-                        <label for="fname">Telefoonnummer</label><sup>*</sup>
-                        <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="0612345678">
-                    </div>
-                    <div class="formgroup">
-                        <label for="fname">Emailadres</label><sup>*</sup>
-                        <input class="form-control form-control-sm" type="text" id="fname" name="firstname" placeholder="Emailadres">
-                    </div>
-                    <div class="formgroup">
-                        <label class="radio">Auto inruilen </label>
-                        <input type="radio" checked="checked" name="radio"> Ja
-                        <input type="radio" name="radio"> Nee
-                        <span class="checkmark"></span>
-                    </div>
-
-                        <button class="btn btn-green">Versturen</button>
-
-
-                </form>
-
+                    </div><!-- end of row -->
+                </div>
             </div>
-        </div>
-        </div>
-    </div>
-
-
     </div>
     </div>
+    </section>
     </div>
-</section>
-</div>
 
 
 
